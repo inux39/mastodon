ca959bb60971e30d7719a401a8b707593b69f186
inux39 <inux39@protonmail.ch>
2026-08-02 01:01:35 +0900

    add tag

--- manifest.bash
+++ manifest.bash
@@ -3,18 +3,29 @@ set -Ceu
 REPO="harbor.inux39.me/private"
 DATE=$(date +%Y%m%d%H%M%S)
 SH_ROOT="$(cd $(dirname "$0"); pwd)"
+ARG="${1-:ALL}"
 
 function manifest() {
   IMAGE="harbor.inux39.me/private/$1"
   TAG="$2"
   echo "[INFO] Start build $IMAGE:$TAG ($REPO/$IMAGE:$TAG)"
   podman manifest create "$IMAGE:$TAG"
-  podman manifest add "$IMAGE:$TAG" "$IMAGE:x86_64"
-  podman manifest add "$IMAGE:$TAG" "$IMAGE:aarch64"
+  podman manifest add "$IMAGE:$TAG" "$IMAGE:${TAG}-x86_64"
+  podman manifest add "$IMAGE:$TAG" "$IMAGE:${TAG}-aarch64"
   podman manifest push "$IMAGE:$TAG"
   echo "[INFO] Complete build $IMAGE:$TAG ($REPO/$IMAGE:$TAG)"
 }
 
-manifest "mastodon" "$1"
-manifest "mastodon-streaming" "$1"
+case "$ARG" in
+  mastodon )
+    manifest "mastodon" "$1"
+    ;;
+  streaming )
+    manifest "streaming" "$1"
+    ;;
+  * )
+    manifest "mastodon" "$1"
+    manifest "streaming" "$1"
+    ;;
+esac
 
